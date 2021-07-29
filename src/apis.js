26974eb2e7a10f3524d00d0ba242b99b3a3a654c
udj70 <uditjain70@gmail.com>
2021-07-29 17:37:57 +0530

    changed

--- src/apis.js
+++ src/apis.js
@@ -1,9 +1,9 @@
-import apitoken from './key';
+import apikey from './key';
 const allNews=()=>{
         return fetch('https://imdb8.p.rapidapi.com/actors/get-all-news?nconst=nm0001667',{
         method:'GET',
         headers:{
-            'x-rapidapi-key':'4d6b5018b6msh280ce260362a620p1c3c5ajsn5249d888909d'
+            'x-rapidapi-key':apikey
 
         }
     })
@@ -17,7 +17,7 @@ const getMovies=(name)=>{
     return fetch('https://imdb8.p.rapidapi.com/title/find?q='+name,{
         method:'GET',
         headers:{
-            'x-rapidapi-key':'4d6b5018b6msh280ce260362a620p1c3c5ajsn5249d888909d'
+            'x-rapidapi-key':apikey
 
         }
     })
